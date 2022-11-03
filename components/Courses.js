1210231222654969fb5a1af9c2fe4912f48825f1
maheralsamman <supermaher2@gmail.com>
2022-11-03 18:21:01 +0100

    add inProgress, courses style

--- components/Courses.js
+++ components/Courses.js
@@ -3,6 +3,7 @@ import content from "../content.json";
 import { useRouter } from "next/router";
 import Books from "./Books";
 
+
 const Courses = () => {
   const { locale } = useRouter();
 
@@ -11,10 +12,10 @@ const Courses = () => {
        <h3 className={`self-center mt-6 uppercase text-black text-2xl ${locale === 'en-US' ? ' tracking-[20px]':''}`}>
        {locale === "en-US" ?  "Courses" : "الدورات"}
       </h3>
-      <div className="grid grid-cols-2 justify-center grid-rows-4 p-2 gap-2 text-white text-sm md:text-xl md:m-8">
+      <div className="grid grid-cols-2 justify-center grid-rows-4 p-2 gap-2 text-white text-sm md:text-xl md:m-8 ">
         {locale === "en-US"
-          ? content.en.courseInfo.map((info) => <p className="text:sm md:text-base p-2 md:w-11/12 md:py-4 md:px-4 border-2 rounded border-[#7849ef81]">- {info}</p>)
-          : content.ar.courseInfo.map((info) => <p className="text:sm md:text-base p-2 md:w-11/12 md:py-4 md:px-4 border-2 rounded border-[#7849ef81]">{info} -</p>)}
+          ? content.en.courseInfo.map((info) => <p className="text:sm md:text-base p-2 md:w-12/12 md:py-4 md:px-4 border-2 rounded border-[#7849ef81] odd:last:col-span-2">- {info}</p>)
+          : content.ar.courseInfo.map((info) => <p className="text:sm md:text-base p-2 md:w-12/12 md:py-4 md:px-4 border-2 rounded border-[#7849ef81] odd:last:col-span-2">{info} -</p>)}
       </div>
           <Books />
     </div>
--- components/InProgress.js
+++ components/InProgress.js
@@ -16,7 +16,7 @@ const InProgress = () => {
       >
         {locale === "en-US" ? "In Progress" : "يجري حاليا"}
       </h3>
-      <div className="grid md:grid-cols-2 md:grid-rows-2 gap-2 p-8 md:gap-8 text-white text-sm md:text-base ">
+      <div className="grid md:grid-cols-2 justify-items-center md:grid-rows-2 gap-2 p-8 md:gap-8 text-white text-sm md:text-base ">
         {locale === "en-US"
           ? content.en.points.map((point) => <p className="p-2 lg:w-9/12 md:py-8 md:px-4 border-2 rounded border-[#7849ef81]">- {point}</p>)
           : content.ar.points.map((point) => <p>{point} -</p>)}
@@ -24,9 +24,11 @@ const InProgress = () => {
       <div className="flex justify-center gap-2 flex-wrap self-center space-x-4">
         {locale === "en-US"
           ? Object.entries(content.en.inprogress).map(([key, value]) => (
-              <div className="flex flex-col w-40 h-50 p-2 justify-start border-2 rounded border-[#7849ef]">
-                <div className="text-[#31ff00]">{key}</div>
-                <div className="text-white">{value}</div>
+              <div className="w-80 h-50 p-2 justify-start border-2 rounded border-[#7849ef]">
+                <p className="inline text-[#31ff00] text:sm md:text-base p-2">{key}:</p>
+                <p className="inline text-white text-xs p-2">{value}</p>
+                {/* <div className="inline text-[#31ff00] text:sm md:text-base p-2">{key}:</div>
+                <div className="inline text-white text-sm p-2">{value}</div> */}
               </div>
             ))
           : Object.entries(content.ar.inprogress).map(([key, value]) => (
