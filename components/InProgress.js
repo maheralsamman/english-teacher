25368c8062693f2d622617fbd7e6965defc2efbd
maheralsamman <supermaher2@gmail.com>
2022-11-08 13:49:27 +0100

    fix inprogres style

--- components/InProgress.js
+++ components/InProgress.js
@@ -21,12 +21,12 @@ const InProgress = () => {
           ? content.en.points.map((point, i) => <p key={i} className="text-sm p-2 w-full md:py-8 md:px-4 border-2 rounded border-[#7849ef81]">- {point}</p>)
           : content.ar.points.map((point, i) => <p key={i} className="text-sm p-2 w-full md:py-8 md:px-4 border-2 rounded border-[#7849ef81]">{point} -</p>)}
       </div> */}
-      <div className="flex justify-center gap-1 flex-wrap self-center">
+      <div className="flex justify-center gap-4 h-full w-full flex-col sm:flex-row sm:flex-wrap content-center items-center  sm:items-unset">
         {locale === "en"
           ? Object.entries(content.en.inprogress).map(([key, value], i) => (
-              <div key={i} className="w-11/12 sm:w-4/12 h-50 p-1 mx-0 border-2 rounded border-[#7849ef]">
-                <p className="inline text-[#31ff00] p-0 text-xs md:text-base">{key}:</p>
-                <p className="inline text-white text-xs p-1">{value}</p>
+              <div key={i} className="w-52 h-64 p-4 flex-col md:flex-row border-2 rounded border-[#7849ef]">
+                <p className="inline text-[#31ff00] p-0 text-xs md:text-lg">{key}:</p>
+                <p className="inline text-white text-xs md:text-base p-1">{value}</p>
               </div>
             ))
           : Object.entries(content.ar.inprogress).map(([key, value], i) => (
